224c9dadafb2fe5e26cb9efa721c58e757acf998
Ryohei Shima <shima01dev@gmail.com>
2020-12-15 12:12:48 +0900

    Add Firebase Client and Admin module.

---
+++ lib/firebase-admin.ts
@@ -0,0 +1,15 @@
+import firebaseAdmin from 'firebase-admin';
+import sa from '../secrets.json';
+
+if (!firebaseAdmin.apps.length) {
+  firebaseAdmin.initializeApp({
+    credential: firebaseAdmin.credential.cert({
+      privateKey: sa.private_key,
+      clientEmail: sa.client_email,
+      projectId: sa.project_id,
+    }),
+    databaseURL: `https://${sa.project_id}.firebaseio.com`,
+  });
+}
+
+export { firebaseAdmin };
